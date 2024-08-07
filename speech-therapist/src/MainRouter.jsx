0d9407722f3ea16c44baf3b1ab59ce749e9ed82c
Dmitrii <114525458+DmitriiM1@users.noreply.github.com>
2024-08-07 14:18:48 +0000

    creating basic components and styles

---
+++ speech-therapist/src/MainRouter.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {Routes, Route} from "react-router-dom";
+import Header from "./components/Header";
+
+
+
+function MainRouter() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+  
+export default MainRouter;
+  
