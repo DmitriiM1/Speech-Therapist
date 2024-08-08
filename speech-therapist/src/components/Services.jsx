0a78424896b0350f27b05ca01f84f9101f42906f
Dmitrii <114525458+DmitriiM1@users.noreply.github.com>
2024-08-08 02:38:22 +0000

    fixed Services component

--- speech-therapist/src/components/Services.jsx
+++ speech-therapist/src/components/Services.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "../styles/services.css";
+import photo from "../assets/header_footerIMG/Colorful Pastel Kindergarten Logo.png"
 
 const Services = () => {
     return (
         <div className="services-container">
-            <div className="circle circle_logo">
-
+            <div className="circle circle_logo1">
+                <img src={photo} alt="" />
             </div>
             <div className="circle circle-large">
                 <h3>ЛОГОПЕД - ДЕФЕКТОЛОГ</h3>
@@ -20,8 +21,8 @@ const Services = () => {
                     <li>Обучение чтению с использованием Кубиков Зайцева</li>
                 </ul>
             </div>
-            <div className="circle circle_logo">
-
+            <div className="circle circle_logo2">
+                <img src={photo} alt="" />
             </div>
             <div className="circle circle-medium">
                 <h3>ПСИХОЛОГ</h3>
@@ -33,14 +34,15 @@ const Services = () => {
                     <li>Ароматерапия</li>
                 </ul>
             </div>
+            <div className="circle circle_logo3">
+
+            </div>
             <div className="circle circle-small">
-                <ul>
-
-
-                    <h3>КОМПЛЕКСНЫЕ ЗАНЯТИЯ ДЛЯ ДЕТЕЙ</h3>
+                <ul className="circle-small-list">
+                    <h5>КОМПЛЕКСНЫЕ ЗАНЯТИЯ ДЛЯ ДЕТЕЙ</h5>
                     <p>(ЛОГОПЕД+ПСИХОЛОГ)</p>
-                    <li><p>Курс СУПЕРМОЗГ</p></li>
-                    <p>развитие межполушарного взаимодействия с элементами мозжечковой стимуляции на доске Бильгоу</p>
+                    <li><span>Курс СУПЕРМОЗГ</span></li>
+                    <span>развитие межполушарного взаимодействия с элементами мозжечковой стимуляции на доске Бильгоу</span>
                 </ul>
             </div>
         </div>
